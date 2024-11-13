e2b89181284c5de188ef48ec7db676e574ba3921
ITAccadh <s10258652@connect.np.edu.sg>
2024-11-13 11:50:58 +0800

    hellpbuttonjs

--- ATM/SQL Setup/report.sql
+++ ATM/SQL Setup/report.sql
@@ -1,8 +1,8 @@
 CREATE TABLE reports (
     id INT PRIMARY KEY IDENTITY(1,1),
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(20) NOT NULL,
-    problem_description TEXT NOT NULL,
-    timestamp DATETIME NOT NULL
-)
+    name VARCHAR(255),
+    email VARCHAR(255),
+    phone_number VARCHAR(20),
+    problem_description TEXT,
+    timestamp DATETIME
+);
